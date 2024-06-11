3bdf982c145113d5f335627f8060a64d99686a01
Atacan BASARAN <atacanbasaran74@gmail.com>
2024-06-11 13:23:21 +0300

    portfolio page design updated

--- frontend/Realtime-Stock-Investment/src/app/portfolio/portfolio.component.ts
+++ frontend/Realtime-Stock-Investment/src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,9 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { StockInvestmentService } from '../services/stock-investment.service';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-portfolio',
@@ -6,12 +11,60 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
-  @Output() portfolioChanged = new EventEmitter<string>();
 
-  portfolios: string[] = ['Portfolio 1', 'Portfolio 2', 'Portfolio 3'];
-  selectedPortfolio: string = this.portfolios[0];
+  constructor(private router: Router, private service: StockInvestmentService)
+  {
+  }
 
-  onPortfolioChange(event: any) {
-    this.portfolioChanged.emit(this.selectedPortfolio);
+  
+  portfolioIds: number[] =[];
+  currentPortfolioId: number = this.portfolioIds[0];
+  stockNames!: string[];
+  filterOptions!: Observable<string[]>;
+  formControl = new FormControl('');
+
+  ngOnInit(){
+    this.service.getUserPortfolioIds().subscribe(response => {
+      if(response.isSuccessful){
+        this.portfolioIds = response.message;
+        this.currentPortfolioId = this.portfolioIds[0];
+        //this.setProfitHistoryChart();
+        //this.setStockDistribution();
+      }
+      else{
+        this.service.showSnackBar(response.message,'error');
+      }
+    });
+
+    this.service.getStockSymbols().subscribe(response => {
+      if(response.isSuccessful){
+        this.stockNames = response.message;
+        this.filterOptions = this.formControl.valueChanges.pipe(
+          startWith(''),map((value: any) => this.filter(value || ''))
+        )
+      }
+      else{
+        this.service.showSnackBar(response.message,'error');
+      }
+    });
+  }
+  addTab() {
+    this.service.addPortfolio().subscribe(response => {
+     if(response.isSuccessful){
+       this.service.getUserPortfolioIds().subscribe(response => {
+        if(response.isSuccessful){
+          this.portfolioIds = response.message;
+        }
+        else{
+          this.service.showSnackBar(response.message,'error');
+        }
+      });
+     }
+    });
+  }
+
+  private filter(value:string) : string[] {
+    const searchValue = value.toLocaleLowerCase();
+    return this.stockNames.filter(option => option.toLocaleLowerCase().includes(searchValue));
   }
 }
